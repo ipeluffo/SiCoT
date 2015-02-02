c754259a7f570ed134cc83d26dad686e3b321d5c
Ignacio Peluffo <ipeluffo@gmail.com>
2015-02-02 12:17:56 -0300

    Home now redirects to Public Stream API

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Home' });
+  // res.render('index', { title: 'Home' });
+  res.redirect('publicStream');
 });
 
 router.get('/publicStream', function (req, res) {
 	res.render('publicStream', { title: 'Public Stream API' });
+});
+
+router.get('/configuration', function (req, res) {
+	res.render('configuration', { title: 'Configuration'});
 });
 
 module.exports = router;
